df447a18b2a1ff0ac990cf942f6546fbf7dec3f2
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-08-24 15:45:55 +0200

    update

--- lc.py
+++ lc.py
@@ -1,6 +1,8 @@
-import enum
-
-
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -16,3 +18,38 @@ class Solution(object):
                 return [num_map[complement], i]
             num_map[num] = i
        
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy_head = ListNode(0)
+        print("dummy_head: ", dummy_head)
+        current = dummy_head  # pointer to base list
+        carry = 0 # carry if addition result > 9
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
+            carry = total // 10
+            current.next = ListNode(total % 10)
+            print("current.next: ", current.next)
+            current = current.next
+            print("current: ", current)
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        
+        return dummy_head.next
+
+### TESTING ###
+s = Solution()
+l1 = None
+l2 = None
+
+print(s.addTwoNumbers(l1, l2))    
