5555afccb4db374d401ef7e6d49245d9832b9803
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-08-24 11:44:27 +0200

    added lc problem

---
+++ lc.py
@@ -0,0 +1,18 @@
+import enum
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # O(n) complexity solutions
+        num_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+       
