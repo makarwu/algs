54b052f9c9d702664cf49cfa46d9818eba5c65f4
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-08-25 13:21:36 +0200

    daily

--- lc.py
+++ lc.py
@@ -25,7 +25,6 @@ class Solution(object):
         :rtype: ListNode
         """
         dummy_head = ListNode(0)
-        print("dummy_head: ", dummy_head)
         current = dummy_head  # pointer to base list
         carry = 0 # carry if addition result > 9
 
@@ -36,9 +35,7 @@ class Solution(object):
             total = val1 + val2 + carry
             carry = total // 10
             current.next = ListNode(total % 10)
-            print("current.next: ", current.next)
             current = current.next
-            print("current: ", current)
 
             if l1:
                 l1 = l1.next
@@ -47,9 +44,50 @@ class Solution(object):
         
         return dummy_head.next
 
-### TESTING ###
-s = Solution()
-l1 = None
-l2 = None
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_map = {}
+        max_length = 0 # result for the max length found
+        start = 0 # start index of current sliding window
+        for i, char in enumerate(s):
+            # If the character is found in the dictionary and its index is within the current window
+            if char in char_map and char_map[char] >= start:
+                # move sliding window to the right
+                start = char_map[char] + 1
 
-print(s.addTwoNumbers(l1, l2))    
+            # Update the last position of the current character
+            char_map[char] = i
+            # calc max length
+            max_length = max(max_length, i - start + 1)
+        return max_length
+
+### TESTING ###
+
+def create_linked_list(lst):
+    dummy_head = ListNode(0)
+    current = dummy_head
+
+    for value in lst:
+        current.next = ListNode(value)
+        current = current.next
+    
+    return dummy_head.next
+
+def print_linked_list(node):
+    values = []
+    while node:
+        values.append(str(node.val))
+        node = node.next
+    print(" -> ".join(values))
+
+l1_values = [2, 4, 3]
+l2_values = [5, 6, 4]
+
+l1 = create_linked_list(l1_values)
+l2 = create_linked_list(l2_values)
+
+s = Solution()
+print_linked_list(s.addTwoNumbers(l1, l2))    
