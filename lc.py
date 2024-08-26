e8f6af8af6baf49f6f478b798f9759fe65da2318
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-08-26 20:02:13 +0200

    p1

--- lc.py
+++ lc.py
@@ -64,6 +64,28 @@ class Solution(object):
             max_length = max(max_length, i - start + 1)
         return max_length
 
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_map = {'I':1, 
+                     'V':5,
+                     'X':10,
+                     'L':50,
+                     'C':100,
+                     'D':500,
+                     'M':1000}
+        result = 0
+        n = len(s)
+        for i in range(n):
+            ### HOW TO AVOID OUT OF BOUNCE !!! ###
+            if i < n-1 and roman_map[s[i]] < roman_map[s[i+1]]:
+                result -= roman_map[s[i]]
+            else:
+                result += roman_map[s[i]]
+        return result
+
 ### TESTING ###
 
 def create_linked_list(lst):
