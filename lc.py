913b351ef391551f18b031196e760c96cd4cbbbd
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-08-27 20:37:50 +0200

    daily

--- lc.py
+++ lc.py
@@ -85,6 +85,18 @@ class Solution(object):
             else:
                 result += roman_map[s[i]]
         return result
+    
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            while strs[i].find(prefix) != 0:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
 
 ### TESTING ###
 
