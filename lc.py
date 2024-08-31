c18f241358134409d63b09620564dbe4dc38f1f8
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-08-31 12:56:13 +0200

    2nd daily

--- lc.py
+++ lc.py
@@ -227,6 +227,33 @@ class Solution(object):
         
         return revers
     
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy_head = ListNode(0)
+        current = dummy_head
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+
+            current = current.next
+        
+        if l1:
+            current.next = l1
+        elif l2:
+            current.next = l2
+        
+        return dummy_head.next
+        
+    
         
 ### TESTING ###
 
