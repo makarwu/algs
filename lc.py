34cc687889de6986e26dc372268323f7d49fba78
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-09-01 11:53:21 +0200

    daily

--- lc.py
+++ lc.py
@@ -252,6 +252,40 @@ class Solution(object):
             current.next = l2
         
         return dummy_head.next
+    
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.strip()
+        if not s:
+            return 0
+        
+        sign = 1
+        index = 0
+
+        if s[0] == "-":
+            sign = -1
+            index += 1
+        elif s[0] == '+':
+            index += 1
+        
+        num = 0
+        while index < len(s) and s[index].isdigit():
+            num = num*10 + int(s[index])
+            index += 1
+        
+        num *= sign
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        if num > INT_MAX:
+            return INT_MAX
+        if num < INT_MIN:
+            return INT_MIN
+              
+        return num
         
     
         
