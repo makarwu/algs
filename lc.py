e150edc840b3196d95ffbd32904c85e5d0e17b0d
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-09-01 12:34:12 +0200

    added 2nd daily

--- lc.py
+++ lc.py
@@ -286,6 +286,22 @@ class Solution(object):
             return INT_MIN
               
         return num
+    
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        j = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                j+=1
+                nums[j] = nums[i]
+
+        return j + 1
         
     
         
