dbee693cd3d552db8eae13c04ac02bc0abfe3715
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-09-03 19:51:10 +0200

    3rd daily

--- lc.py
+++ lc.py
@@ -388,6 +388,32 @@ class Solution(object):
             
         return i
     
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        closest_sum = 2**31 - 1
+        
+        for i in range(len(nums)-2):
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right]
+                
+                if abs(current_sum - target) < abs(closest_sum - target):
+                    closest_sum = current_sum
+                    
+                if current_sum == target:
+                    return current_sum
+                elif current_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+                
+        return closest_sum
         
 ### TESTING ###
 
