63f0e0240680fc912aaa0d6b38c597342e82c92b
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-09-03 12:04:45 +0200

    added daily

--- lc.py
+++ lc.py
@@ -344,6 +344,34 @@ class Solution(object):
                 k += 1
         return k
     
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        val = [
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I')
+        ]
+        roman_int = ""
+        for value, roman in val:
+            while num >= value:
+                roman_int += roman
+                num -= value
+
+        return roman_int
+    
         
 ### TESTING ###
 
