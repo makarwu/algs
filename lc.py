2f9e85430efb5e1d2672f5b7e6d86fd168ef134e
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-09-04 22:15:31 +0200

    added daily

--- lc.py
+++ lc.py
@@ -414,6 +414,28 @@ class Solution(object):
                     right -= 1
                 
         return closest_sum
+    
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        # Two-Pointer approach
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+        
+        while left < right:
+            curr_area = min(height[left], height[right]) * (right - left)
+            max_area = max(max_area, curr_area)
+            
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+            
+        return max_area
+        
         
 ### TESTING ###
 
