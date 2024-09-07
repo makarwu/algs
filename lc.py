7b3528f1e893c6725b430f7cc9089228c9bf5461
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-09-07 17:05:44 +0200

    added daily

--- lc.py
+++ lc.py
@@ -478,7 +478,34 @@ class Solution(object):
         second.next = second.next.next
         
         return dummy.next
+    
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if digits == "":
+            return []
         
+        number_map = {"2" : "abc",
+                     "3": "def",
+                     "4": "ghi",
+                     "5": "jkl",
+                     "6": "mno",
+                     "7": "pqrs",
+                     "8": "tuv",
+                     "9": "wxyz"}
+        
+        char_list = [""]
+        for digit in digits:
+            digit_to_chars = []
+            for product in char_list:
+                for char in number_map[digit]:
+                    digit_to_chars.append(product + char)
+                    
+            char_list = digit_to_chars
+        
+        return char_list
         
 ### TESTING ###
 
