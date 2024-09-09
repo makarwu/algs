4281e26955f768b4a764c1b10595aaa8b4e349ef
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-09-09 22:21:40 +0200

    daily

--- lc.py
+++ lc.py
@@ -211,6 +211,7 @@ class Solution(object):
                     right -= 1 
         return result
     
+    
     def reverse_integer(self, x):
         """
         :type x: int
@@ -546,6 +547,49 @@ class Solution(object):
                 res.remove(i)
         
         return res
+    
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = []
+        l = len(nums)
+        
+        for i in range(l-3):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            for j in range(i+1, l-2):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                
+                left, right = j+1, l-1
+                
+                while left < right:
+                    current_sum = nums[i] + nums[j] + nums[left] + nums[right]
+                    
+                    if current_sum == target:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                    
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                    
+                        
+                        left += 1
+                        right -= 1
+                    
+                    elif current_sum < target:
+                        left += 1
+                    else:
+                        right -= 1
+                        
+        return result
+            
         
 ### TESTING ###
 
