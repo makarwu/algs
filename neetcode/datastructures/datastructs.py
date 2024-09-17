7a082d2c85a4ae7a8a51a80f5be5ad1856ee3c61
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-09-17 14:21:26 +0200

    daily

--- neetcode/datastructures/datastructs.py
+++ neetcode/datastructures/datastructs.py
@@ -31,7 +31,7 @@ class DynamicArray:
         new_arr = [''] * self.capacity
         for i in range(self.size):
             new_arr[i] = self.arr[i]
-            
+
         self.arr = new_arr
 
     def getSize(self) -> int:
@@ -39,3 +39,68 @@ class DynamicArray:
         
     def getCapacity(self) -> int:
         return self.capacity
+
+### (2) Linked-List ###
+
+class Node:
+    def __init__(self, value=0, next=None):
+        self.value = value
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def get(self, index: int) -> int:
+        current = self.head
+        i = 0
+        while current:
+            if i == index:
+                return current.value
+            current = current.next
+            i+=1
+        return -1
+
+    def insertHead(self, val: int) -> None:
+        new_node = Node(val)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insertTail(self, val: int) -> None:
+        new_node = Node(val)
+        current = self.head
+        if not self.head:
+            self.head = new_node
+            return
+            
+        while current.next:
+            current = current.next
+        current.next = new_node
+        
+    def remove(self, index: int) -> bool:
+        if not self.head:
+            return False
+        if index == 0:
+            self.head = self.head.next
+            return True
+
+        current = self.head
+        i = 0
+        while current and current.next:
+            if i == index-1:
+                current.next = current.next.next
+                return True
+            current = current.next
+            i += 1
+
+        return False
+
+
+    def getValues(self) -> List[int]:
+        current = self.head
+        values = []
+        while current:
+            val = current.value
+            values.append(val)
+            current = current.next
+        return values
