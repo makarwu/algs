163163b0eb3221bb44e63a1aab53485afccc2a74
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-09-18 17:11:54 +0200

    lc daily

--- lc.py
+++ lc.py
@@ -796,6 +796,18 @@ class Solution(object):
 
         backtrack(target, [], 0)
         return result
+    
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        for i in range(len(matrix)):
+            for j in range(i, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        for i in range(len(matrix)):
+            matrix[i] = reversed(matrix[i])
 
         
 ### TESTING ###
