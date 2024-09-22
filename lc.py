5cf5ce9e884a8f040c5c8d57d7e69e0c94500ec0
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-09-22 15:04:23 +0200

    2nd daily

--- lc.py
+++ lc.py
@@ -921,7 +921,42 @@ class Solution(object):
             return True
             
         backtrack(board)
-
+    
+    # MEMORY LIMIT EXCEEDED for large n
+    def findKthNumber(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        nums = [str(i) for i in range(1,n+1)]
+        sorted_nums = sorted(nums, key=str)
+        sorted_nums_int = [int(i) for i in sorted_nums]
+        return sorted_nums_int[k-1]
+    
+    # BETTER SOLUTION: PREFIX-BASED TREE TRAVERSAL (SIMILAR TO A TRIE)
+    def findKthNumber2(self, n, k):
+        def countSteps(n, curr, next):
+            steps = 0
+            while curr <= n:
+                steps += min(n + 1, next) - curr
+                curr *= 10
+                next *= 10
+            return steps
+        
+        curr = 1
+        k -= 1  
+        
+        while k > 0:
+            steps = countSteps(n, curr, curr + 1)
+            if steps <= k:
+                curr += 1
+                k -= steps
+            else:
+                curr *= 10
+                k -= 1
+                
+        return curr
         
 ### TESTING ###
 
