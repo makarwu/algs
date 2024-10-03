b397f47c5fe72edc6df6671ce3d0102c69d90d8d
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-10-03 17:03:17 +0200

    daily

--- lc.py
+++ lc.py
@@ -1169,6 +1169,19 @@ class Solution(object):
             col_st+=1
         
         return matrix
+    
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[1]*n for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
+        
 
 
 ### TESTING ###
