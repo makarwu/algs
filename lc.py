2c20ead28033d3ae4ff47816aad69255f84de510
Makar Wuckert <makarwuckert.1@web.de>
2024-10-05 00:48:17 +0200

    daily

--- lc.py
+++ lc.py
@@ -1181,12 +1181,34 @@ class Solution(object):
             for j in range(1, n):
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[m-1][n-1]
-        
+
     def uniquePathsWithObstacles(self, obstacleGrid):
         """
         :type obstacleGrid: List[List[int]]
         :rtype: int
         """
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+
+        if obstacleGrid[0][0] == 1 or obstacleGrid[m-1][n-1] == 1:
+            return 0
+
+        obstacleGrid[0][0] = 1
+
+        for i in range(1, m):
+            obstacleGrid[i][0] = 1 if obstacleGrid[i][0] == 0 and obstacleGrid[i-1][0] == 1 else 0
+
+        for j in range(1, n):
+            obstacleGrid[0][j] = 1 if obstacleGrid[0][j] == 0 and obstacleGrid[0][j-1] == 1 else 0
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 1:
+                    obstacleGrid[i][j] = 0
+                else:
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+
+        return obstacleGrid[m-1][n-1]
 
 
 ### TESTING ###
