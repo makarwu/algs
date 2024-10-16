782a417f38dd8c118148c89a512172668dcfe6ba
Makar Wuckert <makarwuckert.1@web.de>
2024-10-16 09:58:44 +0200

    daily

--- lc.py
+++ lc.py
@@ -1482,6 +1482,19 @@ class Solution(object):
                 i+=1
                 
         return i
+    
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        current = head
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next        
+        return head
 
 
 ### TESTING ###
